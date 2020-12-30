2017960e430149901137c4252cceb84349974744
Anton Krasovsky <anton.krasovsky@gmail.com>
2020-12-30 22:56:42 +0000

    Fix filename resolution in tests

--- src/test/suite/edit.delete.test.ts
+++ src/test/suite/edit.delete.test.ts
@@ -3,50 +3,47 @@ import * as vscode from 'vscode';
 import { readFileSync } from 'fs';
 import { withRandomFileEditor } from '../utils';
 import { deleteJsonNode, deleteYamlNode, safeParse } from '../../util';
+import { resolve } from 'path';
 
 suite('Edit Delete Node Test Suite', () => {
+  test('Methos deleteJsonNode test', async () => {
+    const text = readFileSync(resolve(__dirname, '../../../tests/xhr.json'), { encoding: 'utf8' });
+    const expected =
+      '{\r\n  "openapi": "3.0.0",\r\n  "servers": [\r\n    {\r\n      "url": "http://jsonplaceholder.typicode.com"\r\n    }\r\n  ]\r\n}\r\n';
 
-	test('Methos deleteJsonNode test', async () => {
+    await withRandomFileEditor(text, async (editor, doc) => {
+      const root = safeParse(editor.document.getText(), editor.document.languageId);
+      const edit = new vscode.WorkspaceEdit();
+      edit.delete(editor.document.uri, deleteJsonNode(editor.document, root, '/info'));
+      edit.delete(editor.document.uri, deleteJsonNode(editor.document, root, '/servers/1'));
+      edit.delete(editor.document.uri, deleteJsonNode(editor.document, root, '/paths'));
 
-		const text = readFileSync('../../tests/xhr.json', { encoding: 'utf8' });
-		const expected = '{\r\n  "openapi": "3.0.0",\r\n  "servers": [\r\n    {\r\n      "url": "http://jsonplaceholder.typicode.com"\r\n    }\r\n  ]\r\n}\r\n';
+      return vscode.workspace.applyEdit(edit).then(() => {
+        assert.ok(doc.isDirty);
+        assert.equal(doc.getText(), expected);
+      });
+    });
+  });
 
-		await withRandomFileEditor(text, async (editor, doc) => {
+  test('Methos deleteYamlNode test', async () => {
+    const text = readFileSync(resolve(__dirname, '../../../tests/xkcd.yaml'), { encoding: 'utf8' });
+    const expected = "swagger: '2.0'\r\nschemes:\r\n  - https\r\nhost: xkcd.com\r\n\r\n";
 
-			const root = safeParse(editor.document.getText(), editor.document.languageId);
-			const edit = new vscode.WorkspaceEdit();
-			edit.delete(editor.document.uri, deleteJsonNode(editor.document, root, '/info'));
-			edit.delete(editor.document.uri, deleteJsonNode(editor.document, root, '/servers/1'));
-			edit.delete(editor.document.uri, deleteJsonNode(editor.document, root, '/paths'));
+    await withRandomFileEditor(text, async (editor, doc) => {
+      const root = safeParse(editor.document.getText(), 'yaml');
+      const edit = new vscode.WorkspaceEdit();
+      edit.delete(editor.document.uri, deleteYamlNode(editor.document, root, '/basePath'));
+      edit.delete(editor.document.uri, deleteYamlNode(editor.document, root, '/info'));
+      edit.delete(editor.document.uri, deleteYamlNode(editor.document, root, '/externalDocs'));
+      edit.delete(editor.document.uri, deleteYamlNode(editor.document, root, '/securityDefinitions'));
+      edit.delete(editor.document.uri, deleteYamlNode(editor.document, root, '/paths'));
+      edit.delete(editor.document.uri, deleteYamlNode(editor.document, root, '/definitions'));
+      edit.delete(editor.document.uri, deleteYamlNode(editor.document, root, '/schemes/0'));
 
-			return vscode.workspace.applyEdit(edit).then(() => {
-				assert.ok(doc.isDirty);
-				assert.equal(doc.getText(), expected);
-			});
-		});
-	});
-
-	test('Methos deleteYamlNode test', async () => {
-
-		const text = readFileSync('../../tests/xkcd.yaml', { encoding: 'utf8' });
-		const expected = 'swagger: \'2.0\'\r\nschemes:\r\n  - https\r\nhost: xkcd.com\r\n\r\n';
-
-		await withRandomFileEditor(text, async (editor, doc) => {
-
-			const root = safeParse(editor.document.getText(), 'yaml');
-			const edit = new vscode.WorkspaceEdit();
-			edit.delete(editor.document.uri, deleteYamlNode(editor.document, root, '/basePath'));
-			edit.delete(editor.document.uri, deleteYamlNode(editor.document, root, '/info'));
-			edit.delete(editor.document.uri, deleteYamlNode(editor.document, root, '/externalDocs'));
-			edit.delete(editor.document.uri, deleteYamlNode(editor.document, root, '/securityDefinitions'));
-			edit.delete(editor.document.uri, deleteYamlNode(editor.document, root, '/paths'));
-			edit.delete(editor.document.uri, deleteYamlNode(editor.document, root, '/definitions'));
-			edit.delete(editor.document.uri, deleteYamlNode(editor.document, root, '/schemes/0'));
-
-			return vscode.workspace.applyEdit(edit).then(() => {
-				assert.ok(doc.isDirty);
-				assert.equal(doc.getText(), expected);
-			});
-		});
-	});
+      return vscode.workspace.applyEdit(edit).then(() => {
+        assert.ok(doc.isDirty);
+        assert.equal(doc.getText(), expected);
+      });
+    });
+  });
 });
--- src/test/suite/edit.rename.test.ts
+++ src/test/suite/edit.rename.test.ts
@@ -3,40 +3,36 @@ import * as vscode from 'vscode';
 import { readFileSync } from 'fs';
 import { withRandomFileEditor } from '../utils';
 import { renameKeyNode, safeParse } from '../../util';
+import { resolve } from 'path';
 
 suite('Edit Rename Key Node Test Suite', () => {
+  test('Methos renameKeyNode (json) test', async () => {
+    const text = readFileSync(resolve(__dirname, '../../../tests/xhr.json'), { encoding: 'utf8' });
+    await withRandomFileEditor(text, async (editor, doc) => {
+      const root = safeParse(editor.document.getText(), editor.document.languageId);
+      const range = renameKeyNode(editor.document, root, '/paths/~1posts/get/responses/200');
+      const edit = new vscode.WorkspaceEdit();
+      edit.replace(editor.document.uri, range, '999');
 
-	test('Methos renameKeyNode (json) test', async () => {
+      return vscode.workspace.applyEdit(edit).then(() => {
+        assert.ok(doc.isDirty);
+        assert.equal(doc.lineAt(23).text.trim(), '"999": {');
+      });
+    });
+  });
 
-		const text = readFileSync('../../tests/xhr.json', { encoding: 'utf8' });
-		await withRandomFileEditor(text, async (editor, doc) => {
+  test('Methos renameKeyNode (yaml) test', async () => {
+    const text = readFileSync(resolve(__dirname, '../../../tests/xkcd.yaml'), { encoding: 'utf8' });
+    await withRandomFileEditor(text, async (editor, doc) => {
+      const root = safeParse(editor.document.getText(), 'yaml');
+      const range = renameKeyNode(editor.document, root, '/paths/~1%7BcomicId%7D~1info.0.json');
+      const edit = new vscode.WorkspaceEdit();
+      edit.replace(editor.document.uri, range, 'foo');
 
-			const root = safeParse(editor.document.getText(), editor.document.languageId);
-			const range = renameKeyNode(editor.document, root, '/paths/~1posts/get/responses/200');
-			const edit = new vscode.WorkspaceEdit();
-			edit.replace(editor.document.uri, range, '999');
-
-			return vscode.workspace.applyEdit(edit).then(() => {
-				assert.ok(doc.isDirty);
-				assert.equal(doc.lineAt(23).text.trim(), '"999": {');
-			});
-		});
-	});
-
-	test('Methos renameKeyNode (yaml) test', async () => {
-
-		const text = readFileSync('../../tests/xkcd.yaml', { encoding: 'utf8' });
-		await withRandomFileEditor(text, async (editor, doc) => {
-
-			const root = safeParse(editor.document.getText(), 'yaml');
-			const range = renameKeyNode(editor.document, root, '/paths/~1%7BcomicId%7D~1info.0.json');
-			const edit = new vscode.WorkspaceEdit();
-			edit.replace(editor.document.uri, range, 'foo');
-
-			return vscode.workspace.applyEdit(edit).then(() => {
-				assert.ok(doc.isDirty);
-				assert.equal(doc.lineAt(39).text.trim(), 'foo:');
-			});
-		});
-	});
+      return vscode.workspace.applyEdit(edit).then(() => {
+        assert.ok(doc.isDirty);
+        assert.equal(doc.lineAt(39).text.trim(), 'foo:');
+      });
+    });
+  });
 });
