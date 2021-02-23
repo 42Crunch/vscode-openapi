1e72089c4cb9c7f806d4f91c9291b80d1827faae
Anton Krasovsky <anton.krasovsky@gmail.com>
2021-02-23 12:38:12 +0000

    Update report

--- webview/main.js
+++ webview/main.js
@@ -3,7 +3,7 @@
 
   const focusLine = function (event) {
     vscode.postMessage({
-      command: 'goToLine',
+      command: "goToLine",
       uri: event.target.dataset.uri,
       line: event.target.dataset.lineNo,
     });
@@ -13,16 +13,22 @@
 
   const copyIssueId = function (event) {
     vscode.postMessage({
-      command: 'copyIssueId',
+      command: "copyIssueId",
       id: event.target.dataset.issueId,
     });
     event.preventDefault();
     event.stopPropagation();
   };
 
+  const toggleDropdown = function (event) {
+    event.currentTarget.querySelector("div.dropdown-content").classList.toggle("show");
+    event.preventDefault();
+    event.stopPropagation();
+  };
+
   const goFullReport = function (event) {
     vscode.postMessage({
-      command: 'goFullReport',
+      command: "goFullReport",
       uri: event.target.dataset.uri,
     });
     event.preventDefault();
@@ -30,11 +36,22 @@
   };
 
   setTimeout(() => {
-    document.body.querySelector('h1').scrollIntoView({ behavior: 'auto', block: 'end' });
-    document.body.querySelectorAll('.focus-line').forEach((e) => e.addEventListener('click', focusLine));
+    document.body.querySelector("h1").scrollIntoView({ behavior: "auto", block: "end" });
+    document.body
+      .querySelectorAll(".focus-line")
+      .forEach((e) => e.addEventListener("click", focusLine));
     // there is only one .go-full-report on the page, but doing querySelectorAll() makes it easier to
     // handle case where there is no element on the page
-    document.body.querySelectorAll('.go-full-report').forEach((e) => e.addEventListener('click', goFullReport));
-    document.body.querySelectorAll('.issue-id').forEach((e) => e.addEventListener('click', copyIssueId));
+    document.body
+      .querySelectorAll(".go-full-report")
+      .forEach((e) => e.addEventListener("click", goFullReport));
+
+    document.body
+      .querySelectorAll(".issue-id")
+      .forEach((e) => e.addEventListener("click", copyIssueId));
+
+    document.body
+      .querySelectorAll("div.dropdown")
+      .forEach((e) => e.addEventListener("click", toggleDropdown));
   }, 10);
 })();
