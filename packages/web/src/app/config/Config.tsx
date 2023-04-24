aa83a16207b074145727fb818d884ce9deed66b5
Anton Krasovsky <anton@no-mad.net>
2023-04-24 15:27:42 +0100

    Move sidebar search layout into own component

--- packages/web/src/app/config/Config.tsx
+++ packages/web/src/app/config/Config.tsx
@@ -1,28 +1,32 @@
-import { useEffect, useState } from "react";
-import styled from "styled-components";
+import { useEffect } from "react";
 import { useForm, FormProvider } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import * as z from "zod";
 
-import { ThemeColorVariables } from "@xliic/common/theme";
 import { Config as ConfigData } from "@xliic/common/config";
 
-import List from "../../components/List";
-import { MagnifyingGlass } from "../../icons";
 import { saveConfig, useFeatureDispatch, useFeatureSelector } from "../../features/config/slice";
 import PlatformConnection from "./PlatformConnection";
 import PlatformServices from "./PlatformServices";
 import ScanDockerImage from "./ScanDockerImage";
 import ScanRuntime from "./ScanRuntime";
+import SearchSidebar from "../../components/layout/SearchSidebar";
 
-const platformSettings = [
-  { id: "platform-connection", label: "Connection" },
-  { id: "platform-services", label: "Services" },
-];
-
-const scanSettings = [
-  { id: "scan-image", label: "Docker image" },
-  { id: "scan-runtime", label: "Runtime" },
+const sections = [
+  {
+    title: "42Crunch Platform",
+    items: [
+      { id: "platform-connection", label: "Connection" },
+      { id: "platform-services", label: "Services" },
+    ],
+  },
+  {
+    title: "Conformance Scan",
+    items: [
+      { id: "scan-image", label: "Docker image" },
+      { id: "scan-runtime", label: "Runtime" },
+    ],
+  },
 ];
 
 export default function Config() {
@@ -37,9 +41,6 @@ export default function Config() {
 
 function ConfigForm({ values }: { values: ConfigData }) {
   const dispatch = useFeatureDispatch();
-
-  const [selected, setSelected] = useState("platform-connection");
-  const [search, setSearch] = useState("");
 
   const schema = z
     .object({
@@ -73,118 +74,24 @@ function ConfigForm({ values }: { values: ConfigData }) {
   }, [isDirty, isValid, isValidating]);
 
   return (
-    <Container>
-      <FormProvider {...methods}>
-        <Sidebar>
-          <Search>
-            <input
-              placeholder="Search"
-              value={search}
-              onChange={(e) => setSearch(e.target.value)}
-            />
-            <MagnifyingGlass />
-          </Search>
-          <Subheader>42Crunch Platform</Subheader>
-          <List
-            selected={selected}
-            setSelected={setSelected}
-            items={platformSettings}
-            filter={search.trim()}
-          />
-          <Subheader>Conformance Scan</Subheader>
-          <List
-            selected={selected}
-            setSelected={setSelected}
-            items={scanSettings}
-            filter={search.trim()}
-          />
-        </Sidebar>
-        <Content>
-          {selected === "platform-connection" && <PlatformConnection />}
-          {selected === "platform-services" && <PlatformServices />}
-          {selected === "scan-image" && <ScanDockerImage />}
-          {selected === "scan-runtime" && <ScanRuntime />}
-        </Content>
-      </FormProvider>
-    </Container>
+    <FormProvider {...methods}>
+      <SearchSidebar
+        sections={sections}
+        defaultSelection="platform-connection"
+        render={(selected) => {
+          return (
+            <>
+              {selected === "platform-connection" && <PlatformConnection />}
+              {selected === "platform-services" && <PlatformServices />}
+              {selected === "scan-image" && <ScanDockerImage />}
+              {selected === "scan-runtime" && <ScanRuntime />}
+            </>
+          );
+        }}
+      />
+    </FormProvider>
   );
 }
-
-const Container = styled.div`
-  display: flex;
-  background-color: var(${ThemeColorVariables.background});
-  height: 100vh;
-  overflow: hidden;
-  > :first-child {
-    width: 240px;
-    overflow-y: auto;
-  }
-  > :last-child {
-    flex: 1;
-    overflow-y: auto;
-  }
-`;
-
-const Content = styled.div`
-  background-color: var(${ThemeColorVariables.computedOne});
-  padding: 16px;
-`;
-
-const Sidebar = styled.div`
-  padding: 16px;
-`;
-
-const Subheader = styled.div`
-  display: flex;
-  align-items: center;
-  height: 36px;
-  padding: 0 8px;
-  font-size: 12px;
-  font-weight: 500;
-  white-space: nowrap;
-  overflow: hidden;
-  text-overflow: ellipsis;
-  color: var(${ThemeColorVariables.disabledForeground});
-`;
-
-const Search = styled.div`
-  display: flex;
-  height: 40px;
-  align-items: center;
-  background-color: var(${ThemeColorVariables.inputBackground});
-  border-radius: 2px;
-  border: 1px solid var(${ThemeColorVariables.border});
-
-  > input {
-    flex: 1;
-    margin-left: 8px;
-    background-color: transparent;
-    border: none;
-    color: var(${ThemeColorVariables.foreground});
-    padding: 4px;
-
-    &::placeholder {
-      color: var(${ThemeColorVariables.inputPlaceholderForeground});
-      font-size: 14px;
-    }
-
-    &:focus {
-      outline: none;
-      // outline: 1px solid var(${ThemeColorVariables.focusBorder});
-    }
-  }
-
-  > svg {
-    width: 16px;
-    height: 16px;
-    fill: var(${ThemeColorVariables.foreground});
-    margin: 8px;
-  }
-
-  &:focus-within {
-    border: 1px solid var(${ThemeColorVariables.focusBorder});
-  }
-`;
 
 export function wrapFormValues(values: ConfigData): ConfigData {
   const mutable = JSON.parse(JSON.stringify(values)) as ConfigData;
---
+++ packages/web/src/components/layout/SearchSidebar.tsx
@@ -0,0 +1,127 @@
+import { ReactNode, useState } from "react";
+import styled from "styled-components";
+
+import { ThemeColorVariables } from "@xliic/common/theme";
+import { MagnifyingGlass } from "../../icons";
+import List, { ListItem } from "../List";
+import React from "react";
+
+export type Section = {
+  title: string;
+  items: ListItem[];
+};
+
+export default function SearchSidebar({
+  render,
+  sections,
+  defaultSelection,
+}: {
+  sections: Section[];
+  defaultSelection: string;
+  render: (selection: string) => ReactNode;
+}) {
+  const [selected, setSelected] = useState(defaultSelection);
+  const [search, setSearch] = useState("");
+  return (
+    <Container>
+      <Sidebar>
+        <Search>
+          <input
+            placeholder="Search"
+            value={search}
+            onChange={(e) => setSearch(e.target.value.trim())}
+          />
+          <MagnifyingGlass />
+        </Search>
+        {sections.map((section: Section) => (
+          <React.Fragment key={section.title}>
+            <Subheader>42Crunch Platform</Subheader>
+            <List
+              selected={selected}
+              setSelected={setSelected}
+              items={section.items}
+              filter={search}
+            />
+          </React.Fragment>
+        ))}
+      </Sidebar>
+      <Content>{render(selected)}</Content>
+    </Container>
+  );
+}
+
+const Container = styled.div`
+  display: flex;
+  background-color: var(${ThemeColorVariables.background});
+  height: 100vh;
+  overflow: hidden;
+  > :first-child {
+    width: 240px;
+    overflow-y: auto;
+  }
+  > :last-child {
+    flex: 1;
+    overflow-y: auto;
+  }
+`;
+
+const Content = styled.div`
+  background-color: var(${ThemeColorVariables.computedOne});
+  padding: 16px;
+`;
+
+const Sidebar = styled.div`
+  padding: 16px;
+`;
+
+const Subheader = styled.div`
+  display: flex;
+  align-items: center;
+  height: 36px;
+  padding: 0 8px;
+  font-size: 12px;
+  font-weight: 500;
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  color: var(${ThemeColorVariables.disabledForeground});
+`;
+
+const Search = styled.div`
+  display: flex;
+  height: 40px;
+  align-items: center;
+  background-color: var(${ThemeColorVariables.inputBackground});
+  border-radius: 2px;
+  border: 1px solid var(${ThemeColorVariables.border});
+
+  > input {
+    flex: 1;
+    margin-left: 8px;
+    background-color: transparent;
+    border: none;
+    color: var(${ThemeColorVariables.foreground});
+    padding: 4px;
+
+    &::placeholder {
+      color: var(${ThemeColorVariables.inputPlaceholderForeground});
+      font-size: 14px;
+    }
+
+    &:focus {
+      outline: none;
+      // outline: 1px solid var(${ThemeColorVariables.focusBorder});
+    }
+  }
+
+  > svg {
+    width: 16px;
+    height: 16px;
+    fill: var(${ThemeColorVariables.foreground});
+    margin: 8px;
+  }
+
+  &:focus-within {
+    border: 1px solid var(${ThemeColorVariables.focusBorder});
+  }
+`;
