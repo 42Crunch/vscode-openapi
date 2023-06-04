8217929b61c1d8dc2973a5282aa36838e4740ea7
Anton Krasovsky <anton@no-mad.net>
2023-06-04 16:50:24 +0100

    Trigger settings form re-validation on every screen change

--- packages/web/src/app/config/ConfigurationForm.tsx
+++ packages/web/src/app/config/ConfigurationForm.tsx
@@ -43,7 +43,7 @@ export default function ConfigurationForm<T extends Partial<Config>>({
 
   useEffect(() => {
     methods.trigger();
-  }, []);
+  }, [id]);
 
   useEffect(() => {
     if (isValid && isDirty) {
--- packages/web/src/app/config/screen/platform-connection.tsx
+++ packages/web/src/app/config/screen/platform-connection.tsx
@@ -50,8 +50,10 @@ const schema = z.object({
   platformApiToken: z
     .string()
     .regex(
-      /^(ide_|api_)?[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i
-    ),
+      /^(ide_|api_)?[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i,
+      "Token is invalid"
+    )
+    .or(z.literal("")),
 });
 
 const screen: {
