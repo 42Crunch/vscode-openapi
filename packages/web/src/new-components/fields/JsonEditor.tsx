d0ad3054396e5e6e099dc9b001fa8988689ddb73
Anton Krasovsky <anton@no-mad.net>
2023-10-20 21:20:47 +0100

    Add status line to JSON plugin

--- packages/web/src/new-components/fields/JsonEditor.tsx
+++ packages/web/src/new-components/fields/JsonEditor.tsx
@@ -7,12 +7,13 @@ import { HistoryPlugin } from "@lexical/react/LexicalHistoryPlugin";
 import { PlainTextPlugin } from "@lexical/react/LexicalPlainTextPlugin";
 import { ThemeColorVariables } from "@xliic/common/theme";
 import { $createLineBreakNode, $createParagraphNode, $getRoot } from "lexical";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useController } from "react-hook-form";
 import styled from "styled-components";
 import { VariableNode } from "./editor/VariableNode";
 import VariablesPlugin from "./editor/VariablesPlugin";
 import { createLineNodes } from "./editor/utils";
+import { CircleCheck, ExclamationCircle } from "../../icons";
 
 export default function JsonEditor({ name, variables }: { name: string; variables: string[] }) {
   const {
@@ -62,13 +63,67 @@ export default function JsonEditor({ name, variables }: { name: string; variable
   );
 }
 
+function FormPlugin({ name }: { name: string }) {
+  const [editor] = useLexicalComposerContext();
+
+  const [error, setError] = useState<string | undefined>(undefined);
+
+  const { field } = useController({
+    name,
+  });
+
+  useEffect(() => {
+    let timeoutId: ReturnType<typeof setTimeout> | undefined = undefined;
+    return editor.registerTextContentListener((text) => {
+      if (timeoutId !== undefined) {
+        clearTimeout(timeoutId);
+      }
+      try {
+        field.onChange(JSON.parse(text));
+        setError(undefined);
+      } catch (e) {
+        timeoutId = setTimeout(() => {
+          if (e instanceof SyntaxError) {
+            setError(e.message);
+          } else {
+            setError(`${e}`);
+          }
+        }, 500);
+      }
+    });
+  }, [editor, field]);
+
+  if (error === undefined) {
+    return (
+      <StatusLine>
+        Valid JSON
+        <CircleCheck
+          style={{
+            fill: `var(${ThemeColorVariables.foreground})`,
+          }}
+        />
+      </StatusLine>
+    );
+  } else {
+    return (
+      <StatusLine>
+        {error}
+        <ExclamationCircle
+          style={{
+            fill: `var(${ThemeColorVariables.errorForeground})`,
+          }}
+        />
+      </StatusLine>
+    );
+  }
+}
+
 const Container = styled.div`
-  padding: 4px;
+  //padding: 4px;
   color: var(${ThemeColorVariables.foreground});
   background-color: var(${ThemeColorVariables.background});
   border: 1px solid var(${ThemeColorVariables.border});
-  font-family: monospace;
-  font-size: 13.333px;
+
   &:focus-within {
     //border: 1px solid var(${ThemeColorVariables.focusBorder});
     outline: 1px solid -webkit-focus-ring-color;
@@ -78,28 +133,24 @@ const Container = styled.div`
     outline: none;
   }
   > .editor {
+    font-family: monospace;
+    font-size: 13.333px;
+    margin: 4px;
   }
   .editor-paragraph {
     margin: 0;
   }
 `;
 
-function FormPlugin({ name }: { name: string }) {
-  const [editor] = useLexicalComposerContext();
-
-  const { field } = useController({
-    name,
-  });
-
-  useEffect(() => {
-    return editor.registerTextContentListener((text) => {
-      try {
-        field.onChange(JSON.parse(text));
-      } catch (e) {
-        console.log("failed to parse");
-      }
-    });
-  }, [editor, field]);
-
-  return null;
-}
+const StatusLine = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+  gap: 4px;
+  padding: 2px 4px;
+  border-top: 1px solid var(${ThemeColorVariables.border});
+  //color: var(${ThemeColorVariables.errorForeground});
+  //background-color: var(${ThemeColorVariables.errorBackground});
+  //padding: 0.75rem;
+  line-break: anywhere;
+`;
