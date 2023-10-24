92a22db24343a5bf33a329994e188adedc477602
Anton Krasovsky <anton@no-mad.net>
2023-10-24 17:20:01 +0100

    Move cli related calls to its own file

---
+++ src/platform/scan/runtime/cli-ast.ts
@@ -0,0 +1,91 @@
+/*
+ Copyright (c) 42Crunch Ltd. All rights reserved.
+ Licensed under the GNU Affero General Public License version 3. See LICENSE.txt in the project root for license information.
+*/
+
+import { Config } from "@xliic/common/config";
+import { SimpleEnvironment } from "@xliic/common/env";
+import { readFileSync, writeFileSync } from "fs";
+import { execFileSync } from "node:child_process";
+import { mkdtempSync } from "node:fs";
+import { homedir, tmpdir } from "node:os";
+import { dirname } from "node:path";
+import { join } from "path";
+import * as vscode from "vscode";
+import { EnvStore } from "../../../envstore";
+import { Logger } from "../../types";
+
+export async function createScanConfig(
+  apiFileName: string,
+  scanconfFileName: string
+): Promise<boolean> {
+  const cwd = dirname(apiFileName);
+  const cli = join(homedir(), ".42crunch", "bin", "42c-ast");
+
+  try {
+    execFileSync(
+      cli,
+      [
+        "scan",
+        "conf",
+        "generate",
+        "--output-format",
+        "json",
+        "--output",
+        scanconfFileName,
+        apiFileName,
+      ],
+      { cwd, windowsHide: true }
+    );
+  } catch (e: any) {
+    vscode.window.showErrorMessage("Failed to create scan config: " + e.message);
+    return false;
+  }
+
+  return true;
+}
+
+export async function runScan(
+  envStore: EnvStore,
+  scanEnv: SimpleEnvironment,
+  config: Config,
+  logger: Logger,
+  oas: string,
+  scanconf: string
+) {
+  logger.info(`Running conformance scan using 42c-ast binary`);
+
+  const tmpDir = tmpdir();
+  const dir = mkdtempSync(join(`${tmpDir}`, "scan-"));
+
+  writeFileSync(join(dir as string, "openapi.json"), oas, { encoding: "utf8" });
+  writeFileSync(join(dir as string, "scanconf.json"), scanconf, { encoding: "utf8" });
+
+  logger.info(`Wrote scan configuration to: ${dir}`);
+
+  const cli = join(homedir(), ".42crunch", "bin", "42c-ast");
+
+  logger.info(`Running scan using: ${cli}`);
+
+  execFileSync(
+    cli,
+    [
+      "scan",
+      "run",
+      "openapi.json",
+      "--conf-file",
+      "scanconf.json",
+      "--output",
+      "report.json",
+      "--output-format",
+      "json",
+      "--verbose",
+      "debug",
+    ],
+    { cwd: dir as string, windowsHide: true, env: scanEnv }
+  );
+
+  const report = readFileSync(join(dir as string, "report.json"), { encoding: "utf8" });
+  const parsed = JSON.parse(report);
+  return parsed;
+}
