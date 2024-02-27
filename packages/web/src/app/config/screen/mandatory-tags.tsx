50432933579e947bfcdd70d491e7324dcf3e6d02
Anton Krasovsky <anton@no-mad.net>
2024-02-27 17:12:32 +0000

    Add description to the mandatory tag screen

--- packages/web/src/app/config/screen/mandatory-tags.tsx
+++ packages/web/src/app/config/screen/mandatory-tags.tsx
@@ -11,6 +11,15 @@ export function MandatoryTags() {
     <>
       <Title>Mandatory Tags</Title>
 
+      <p>
+        42Crunch platform mandatory tags, these tags will be added to every API created on the
+        platform.
+      </p>
+      <p>
+        Tags are specified in the format <code>category:tag</code>, and multiple tags are separated
+        by spaces or commas.
+      </p>
+
       <Container>
         <Textarea label="Tags" name="platformMandatoryTags" />
       </Container>
