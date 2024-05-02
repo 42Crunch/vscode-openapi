100269f9cc94b081df21724fb92ef209421f9e74
Anton Krasovsky <anton@no-mad.net>
2024-05-02 15:25:30 +0100

    Move 'Scan all operations' button to the Operations view

--- packages/web/src/app/scanconf/operations/Operations.tsx
+++ packages/web/src/app/scanconf/operations/Operations.tsx
@@ -1,23 +1,13 @@
-import { serialize } from "@xliic/scanconf";
-import { Playbook } from "@xliic/scanconf";
-
 import { SearchSidebarControlled } from "../../../components/layout/SearchSidebar";
-import Button from "../../../new-components/Button";
 import { useAppDispatch, useAppSelector } from "../store";
 import Operation from "./Operation";
 import { setOperationId } from "./slice";
-import { runFullScan } from "../actions";
-import { optionallyReplaceLocalhost } from "./util";
-import { makeEnvEnv } from "../../../core/playbook/execute";
 
 export default function Operations() {
   const dispatch = useAppDispatch();
   const operationId = useAppSelector((state) => state.operations.operationId);
   const onSetOperationId = (operationId: string) => dispatch(setOperationId(operationId));
-  const { oas, playbook, servers } = useAppSelector((state) => state.scanconf);
-  const config = useAppSelector((state) => state.config.data);
-  const env = useAppSelector((state) => state.env.data);
-  const preferredScanServer = useAppSelector((state) => state.prefs.scanServer);
+  const { playbook } = useAppSelector((state) => state.scanconf);
 
   const items = Object.keys(playbook.operations).map((id) => ({ id, label: id }));
 
@@ -29,34 +19,6 @@ export default function Operations() {
     },
   ];
 
-  const runScan = (server: string) => {
-    const updatedServer = optionallyReplaceLocalhost(
-      server,
-      config.scanRuntime,
-      config.docker.replaceLocalhost,
-      config.platform
-    );
-
-    const [serialized, error] = serialize(oas, playbook);
-    if (error !== undefined) {
-      console.log("failed to serialize", error);
-      // FIXME show error when serializing
-      return;
-    }
-
-    const { simple } = makeEnvEnv(Playbook.getCurrentEnvironment(playbook), env);
-
-    dispatch(
-      runFullScan({
-        env: {
-          SCAN42C_HOST: updatedServer,
-          ...simple,
-        },
-        scanconf: JSON.stringify(serialized, null, 2),
-      })
-    );
-  };
-
   return (
     <SearchSidebarControlled
       title="operations"
@@ -64,18 +26,6 @@ export default function Operations() {
       selected={operationId ? { sectionId: "operations", itemId: operationId } : undefined}
       sections={sections}
       onSelected={(selected) => onSetOperationId(selected.itemId)}
-      renderButtons={() => (
-        <Button
-          style={{ width: "100%" }}
-          onClick={(e) => {
-            e.preventDefault();
-            e.stopPropagation();
-            runScan(preferredScanServer || servers[0]);
-          }}
-        >
-          Scan all operations
-        </Button>
-      )}
       render={(selected) => {
         if (selected !== undefined)
           return <Operation operationId={selected.itemId} key={selected.itemId} />;
--- packages/web/src/app/scanconf/requests/Requests.tsx
+++ packages/web/src/app/scanconf/requests/Requests.tsx
@@ -1,22 +1,28 @@
 import { HttpMethod } from "@xliic/openapi";
-import { Playbook } from "@xliic/scanconf";
+import { Playbook, serialize } from "@xliic/scanconf";
 
 import { ItemId, SearchSidebarControlled } from "../../../components/layout/SearchSidebar";
 import { Menu, MenuItem } from "../../../new-components/Menu";
+import Button from "../../../new-components/Button";
 import { removeRequest, saveRequest } from "../slice";
 import { useAppDispatch, useAppSelector } from "../store";
 import AddExternalRequestDialog from "./AddExternalRequestDialog";
 import Request from "./Request";
 import { setRequestId } from "./slice";
+import { runFullScan } from "../actions";
+import { optionallyReplaceLocalhost } from "../operations/util";
+import { makeEnvEnv } from "../../../core/playbook/execute";
 
 export default function Operations() {
   const dispatch = useAppDispatch();
 
-  const {
-    playbook: { operations, requests },
-  } = useAppSelector((state) => state.scanconf);
+  const { oas, playbook, servers } = useAppSelector((state) => state.scanconf);
 
   const requestRef = useAppSelector((state) => state.requests.ref);
+
+  const config = useAppSelector((state) => state.config.data);
+  const env = useAppSelector((state) => state.env.data);
+  const preferredScanServer = useAppSelector((state) => state.prefs.scanServer);
 
   const onSetOperationId = ({ sectionId, itemId }: { sectionId: string; itemId: string }) => {
     const type = sectionId === "operation" ? "operation" : "request";
@@ -26,7 +32,7 @@ export default function Operations() {
   const onRemoveRequest = (id: string) => {
     if (requestRef?.type === "request" && requestRef.id === id) {
       // removing currently selected request, reset selection to the first available operation
-      const firstOperationId = Object.keys(operations)?.[0];
+      const firstOperationId = Object.keys(playbook.operations)?.[0];
       if (firstOperationId !== undefined) {
         dispatch(setRequestId({ type: "operation", id: firstOperationId }));
       }
@@ -34,13 +40,13 @@ export default function Operations() {
     dispatch(removeRequest({ type: "request", id }));
   };
 
-  const operationItems = Object.keys(operations).map((id) => ({ id, label: id }));
+  const operationItems = Object.keys(playbook.operations).map((id) => ({ id, label: id }));
 
-  const requestItems = Object.entries(requests || {})
+  const requestItems = Object.entries(playbook.requests || {})
     .filter(([id, request]) => request.operationId !== undefined)
     .map(([id, request]) => ({ id, label: id }));
 
-  const externalRequestItems = Object.entries(requests || {})
+  const externalRequestItems = Object.entries(playbook.requests || {})
     .filter(([id, request]) => request.operationId === undefined)
     .map(([id, request]) => ({
       id,
@@ -55,10 +61,10 @@ export default function Operations() {
     }));
 
   let selected: ItemId | undefined = undefined;
-  if (requestRef?.type === "operation" && operations[requestRef.id] !== undefined) {
+  if (requestRef?.type === "operation" && playbook.operations[requestRef.id] !== undefined) {
     selected = { itemId: requestRef.id, sectionId: "operation" };
-  } else if (requestRef?.type === "request" && requests?.[requestRef.id] !== undefined) {
-    if (requests[requestRef.id].operationId !== undefined) {
+  } else if (requestRef?.type === "request" && playbook.requests?.[requestRef.id] !== undefined) {
+    if (playbook.requests[requestRef.id].operationId !== undefined) {
       selected = { itemId: requestRef.id, sectionId: "requests" };
     } else {
       selected = { itemId: requestRef.id, sectionId: "external" };
@@ -130,12 +136,52 @@ export default function Operations() {
     },
   ];
 
+  const runScan = (server: string) => {
+    const updatedServer = optionallyReplaceLocalhost(
+      server,
+      config.scanRuntime,
+      config.docker.replaceLocalhost,
+      config.platform
+    );
+
+    const [serialized, error] = serialize(oas, playbook);
+    if (error !== undefined) {
+      console.log("failed to serialize", error);
+      // FIXME show error when serializing
+      return;
+    }
+
+    const { simple } = makeEnvEnv(Playbook.getCurrentEnvironment(playbook), env);
+
+    dispatch(
+      runFullScan({
+        env: {
+          SCAN42C_HOST: updatedServer,
+          ...simple,
+        },
+        scanconf: JSON.stringify(serialized, null, 2),
+      })
+    );
+  };
+
   return (
     <SearchSidebarControlled
-      title="requests"
+      title="operations"
       selected={selected}
       sections={sections}
       onSelected={onSetOperationId}
+      renderButtons={() => (
+        <Button
+          style={{ width: "100%" }}
+          onClick={(e) => {
+            e.preventDefault();
+            e.stopPropagation();
+            runScan(preferredScanServer || servers[0]);
+          }}
+        >
+          Scan all operations
+        </Button>
+      )}
       hideEmptySections={true}
       render={(selected) => {
         if (selected !== undefined)
