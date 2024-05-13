f1b7490dc4fe77522b376091524048540681523d
Anton Krasovsky <anton@no-mad.net>
2024-05-13 12:33:46 +0100

    Tweak button label

--- packages/web/src/app/config/screen/scan-runtime.tsx
+++ packages/web/src/app/config/screen/scan-runtime.tsx
@@ -137,7 +137,7 @@ export function PlatformServices() {
           <>
             <Test>
               <ValidProgressButton
-                label="Download 42Crunch API Security Testing Binary"
+                label="Download"
                 waiting={waitingForCliDownload}
                 onClick={(e) => {
                   dispatch(downloadCli());
