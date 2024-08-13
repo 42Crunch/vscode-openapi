ccd66334546b4362d2a9f32421c1caa947d86a92
Anton Krasovsky <anton@no-mad.net>
2024-08-13 13:35:55 +0100

    Remove old signup UI code

--- src/credentials.ts
+++ src/credentials.ts
@@ -34,7 +34,7 @@ export async function ensureHasCredentials(
   configuration: Configuration,
   secrets: vscode.SecretStorage
 ): Promise<boolean> {
-  const credentials = undefined; //await hasCredentials(configuration, secrets);
+  const credentials = await hasCredentials(configuration, secrets);
   if (credentials === undefined) {
     // try asking for credentials if not found
     const configured = await configureCredentials(signUpWebView);
@@ -42,7 +42,6 @@ export async function ensureHasCredentials(
       // or don't do audit if no credentials been supplied
       return false;
     } else {
-      await delay(3000);
       return true;
     }
   }
@@ -83,135 +82,4 @@ export async function configureCredentials(signUpWebView: SignUpWebView): Promis
   return new Promise<TokenType>((resolve, _reject) => {
     signUpWebView.showSignUp(resolve);
   });
-}
-
-async function chooseNewOrExisting(): Promise<"existing" | "new" | undefined> {
-  await delay(100); // workaround for #133073
-  const options = [
-    "I have an existing 42Crunch Platform account",
-    "I'm a new user, please email me the token",
-  ];
-  const response = await vscode.window.showInformationMessage(
-    "VS Code needs an API key to use the service.",
-    {
-      detail:
-        "42Crunch Audit runs 300+ checks for security best practices in your API. Use your existing platform credentials or provide an email to receive a freemium token.",
-      modal: true,
-    },
-    ...options
-  );
-  if (response === options[0]) {
-    return "existing";
-  }
-  if (response === options[1]) {
-    return "new";
-  }
-  return undefined;
-}
-
-async function configureAnondUser(configuration: Configuration): Promise<boolean> {
-  const email = await vscode.window.showInputBox({
-    prompt: "Enter your email to receive the token.",
-    ignoreFocusOut: true,
-    placeHolder: "email address",
-    validateInput: (value) =>
-      value.indexOf("@") > 0 && value.indexOf("@") < value.length - 1
-        ? null
-        : "Please enter valid email address",
-  });
-
-  if (!email) {
-    return false;
-  }
-
-  const tokenRequestResult = await vscode.window.withProgress(
-    { location: vscode.ProgressLocation.Notification, title: "Requesting token" },
-    async (progress, token) => {
-      try {
-        return await requestToken(email);
-      } catch (e) {
-        vscode.window.showErrorMessage("Unexpected error when trying to request token: " + e);
-      }
-    }
-  );
-
-  if (!tokenRequestResult || tokenRequestResult.status !== "success") {
-    return false;
-  }
-
-  const token = await vscode.window.showInputBox({
-    prompt:
-      "The token has been sent. If you don't get the mail within a couple minutes, check your spam folder and that the address is correct. Paste the token above.",
-    ignoreFocusOut: true,
-    placeHolder: "token",
-  });
-
-  if (!token) {
-    return false;
-  }
-
-  await configuration.update("securityAuditToken", token, vscode.ConfigurationTarget.Global);
-  await configuration.update("platformAuthType", "anond-token", vscode.ConfigurationTarget.Global);
-
-  return true;
-}
-
-export async function configurePlatformUser(
-  configuration: Configuration,
-  secrets: vscode.SecretStorage
-): Promise<boolean> {
-  const platformUrl = await vscode.window.showInputBox({
-    prompt: "Enter 42Crunch platform URL",
-    placeHolder: "platform url",
-    value: "https://platform.42crunch.com/",
-    ignoreFocusOut: true,
-    validateInput: (input) => {
-      try {
-        const url = vscode.Uri.parse(input, true);
-        if (url.scheme !== "https") {
-          return 'URL scheme must be "https"';
-        }
-        if (!url.authority) {
-          return "URL authority must not be empty";
-        }
-        if (url.path != "/") {
-          return "URL path must be empty";
-        }
-      } catch (ex) {
-        return `${ex}`;
-      }
-    },
-  });
-
-  if (!platformUrl) {
-    return false;
-  }
-
-  const UUID_REGEX =
-    /^(ide_|api_)?[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
-  const token = await vscode.window.showInputBox({
-    prompt: "Enter 42Crunch IDE token",
-    placeHolder: "IDE Token",
-    ignoreFocusOut: true,
-    validateInput: (input) => {
-      if (!input || !input.match(UUID_REGEX)) {
-        return "Must be a valid IDE Token";
-      }
-    },
-  });
-
-  if (!token) {
-    return false;
-  }
-
-  await configuration.update(
-    "platformUrl",
-    vscode.Uri.parse(platformUrl).toString(),
-    vscode.ConfigurationTarget.Global
-  );
-
-  await secrets.store("platformApiToken", token);
-  await configuration.update("platformAuthType", "api-token", vscode.ConfigurationTarget.Global);
-
-  return true;
 }
