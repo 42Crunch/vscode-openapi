03c7928c6bbe22339bc47ecbddcafddf362802c3
Anton Krasovsky <anton@no-mad.net>
2024-10-21 12:44:37 +0100

    Update eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -8,4 +8,10 @@ export default [
   { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
+  {
+    rules: {
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/no-unused-vars": "off",
+    },
+  },
 ];
