13049c3c3fbebf0a9944be38de0fe5d78b448ecb
Viacheslav Fateev <viacheslav.fateev@gmail.com>
2025-02-13 10:02:47 +0300

    Draft2

--- packages/web/src/app/bigfiles/slice.ts
+++ packages/web/src/app/bigfiles/slice.ts
@@ -2,6 +2,7 @@ import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 
 //const { getNewParser } = require("./clarinet.js");
 import { getNewParser } from "./getNewParser";
+import { demo } from "./indexedDb";
 var exports = {};
 getNewParser(exports);
 const parser = (exports as any).parser();
@@ -33,12 +34,14 @@ export interface CaptureState {
   file: string | undefined;
   chunkSize: number;
   counter: number;
+  text: string;
 }
 
 const initialState: CaptureState = {
   file: undefined,
   chunkSize: 0,
   counter: 0,
+  text: "",
 };
 
 export const slice = createSlice({
@@ -60,7 +63,18 @@ export const slice = createSlice({
     ) => {
       state.chunkSize = action.payload.textSegment.length;
       state.counter += 1;
-      parser.write(action.payload.textSegment);
+      state.text += action.payload.textSegment;
+      if (action.payload.progress === 1.0) {
+        // TEST INDEX DB
+        const parsed = JSON.parse(state.text);
+        if (parsed.data?.data) {
+          demo(parsed.data);
+        } else {
+          demo(parsed);
+        }
+      }
+      // TEST PARSER
+      //parser.write(action.payload.textSegment);
       //console.info("exports = " + exports);
     },
   },
