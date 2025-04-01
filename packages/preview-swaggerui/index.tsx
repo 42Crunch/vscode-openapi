096a21d7fcebd2d4137e9bb2bc48a4c6b1f73ea5
Anton Krasovsky <anton@no-mad.net>
2025-04-01 12:56:29 +0100

    Fix several swagger-ui preview issues by rendering it using OAS url instead of spec

--- packages/preview-swaggerui/index.tsx
+++ packages/preview-swaggerui/index.tsx
@@ -3,4 +3,24 @@ import "./styles.css";
 
 import { Preview } from "./preview";
 
+const originalFetch = globalThis.fetch;
+
+globalThis.fetch = (input: RequestInfo | URL, init?: RequestInit) => {
+  const url = input instanceof URL ? input.toString() : input.toString();
+
+  if (url.startsWith("http://openapi-preview.example/")) {
+    return Promise.resolve(
+      new Response(globalThis["previewPayload"]! as string, {
+        status: 200,
+        headers: {
+          "Content-Type": "application/json",
+        },
+      })
+    );
+  }
+
+  // Pass through all other requests to the original fetch
+  return originalFetch(input, init);
+};
+
 ReactDOM.render(<Preview />, document.getElementById("root"));
--- packages/preview-swaggerui/preview.tsx
+++ packages/preview-swaggerui/preview.tsx
@@ -1,7 +1,7 @@
-import * as React from 'react';
-import { useEffect, useState } from 'react';
-import SwaggerUI from 'swagger-ui-react';
-import 'swagger-ui-react/swagger-ui.css';
+import * as React from "react";
+import { useEffect, useState } from "react";
+import SwaggerUI from "swagger-ui-react";
+import "swagger-ui-react/swagger-ui.css";
 
 declare var acquireVsCodeApi: any;
 
@@ -11,21 +11,22 @@ export const Preview = () => {
   useEffect(() => {
     const vscode = acquireVsCodeApi();
 
-    window.addEventListener('message', (event) => {
+    window.addEventListener("message", (event) => {
       const message = event.data;
       switch (message.command) {
-        case 'preview':
+        case "preview":
+          globalThis["previewPayload"] = message.text;
           const oas = JSON.parse(message.text);
           setOas(oas);
           break;
       }
     });
-    vscode.postMessage({ command: 'init' });
+    vscode.postMessage({ command: "init" });
   }, []);
 
   if (!oas) {
     return <p>Loading...</p>;
   }
 
-  return <SwaggerUI spec={oas} />;
+  return <SwaggerUI url="http://openapi-preview.example/" />;
 };
