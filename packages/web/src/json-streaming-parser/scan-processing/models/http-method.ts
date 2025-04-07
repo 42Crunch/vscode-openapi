7ce09080e3c4bfb1cfd63268d01d9263d4c6e8f3
Viacheslav Fateev <viacheslav.fateev@gmail.com>
2025-04-07 16:14:35 +0300

    Draft4

---
+++ packages/web/src/json-streaming-parser/scan-processing/models/http-method.ts
@@ -0,0 +1,11 @@
+export enum HttpMethod {
+  Get = 'GET',
+  Put = 'PUT',
+  Post = 'POST',
+  Delete = 'DELETE',
+  Options = 'OPTIONS',
+  Head = 'HEAD',
+  Patch = 'PATCH',
+  Trace = 'TRACE',
+  Connect = 'CONNECT'
+}
--- src/webapps/views/config/activate.ts
+++ src/webapps/views/config/activate.ts
@@ -9,6 +9,7 @@ import { ConfigWebView } from "./view";
 import { PlatformStore } from "../../../platform/stores/platform-store";
 import { Logger } from "../../../platform/types";
 import { BigFilesWebView } from "../bigfiles/view";
+import { ScanReportWebView } from "../../../platform/scan/report-view";
 
 export function activate(
   context: vscode.ExtensionContext,
@@ -18,13 +19,49 @@ export function activate(
   logger: Logger
 ) {
   //const view = new ConfigWebView(context.extensionPath, configuration, secrets, platform, logger);
-  const view = new BigFilesWebView(context.extensionPath, configuration);
+  const view = new ScanReportWebView(
+    `Scan report`,
+    context.extensionPath,
+    cache,
+    configuration,
+    secrets,
+    store,
+    envStore,
+    prefs
+  );
 
   vscode.commands.registerCommand("openapi.showConfiguration", async () => {
-    await view.showBigFilesWebView();
+    await startScan();
   });
 
   vscode.commands.registerCommand("openapi.showSettings", async () => {
-    await view.showBigFilesWebView();
+    await startScan();
   });
+}
+
+async function startScan() {
+  const uri = await vscode.window.showOpenDialog({
+    title: "Select Scan Report",
+    canSelectFiles: true,
+    canSelectFolders: false,
+    canSelectMany: false,
+  });
+  if (uri) {
+    const file = payload.file;
+    const report = await readFile(file, { encoding: "utf8" });
+    const n = 10;
+    let offset = 0;
+    let chunkSize = Math.ceil(report.length / n);
+    for (let i = 1; i <= n; i++) {
+      if (report.length - offset < chunkSize) {
+        chunkSize = report.length - offset;
+      }
+      const textSegment = report.substr(offset, chunkSize);
+      offset += chunkSize;
+      this.sendRequest({
+        command: "sendFileSegment",
+        payload: { file, textSegment, progress: i / n },
+      });
+    }
+  }
 }
