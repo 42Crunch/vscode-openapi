617312b379ee61c5f67515cdc9a7465998b4903d
Viacheslav Fateev <viacheslav.fateev@gmail.com>
2025-06-10 16:26:05 +0300

    Draft5

--- packages/web/src/json-streaming-parser/scanv2-processor.ts
+++ packages/web/src/json-streaming-parser/scanv2-processor.ts
@@ -1,3 +1,4 @@
+import { IndexStore } from "@xliic/streaming-parser/src";
 import { Scanv2Db } from "./scanv2.db";
 
 let dbService: Scanv2Db;
@@ -22,6 +23,9 @@ export async function initScanv2Db(dbName: string): Promise<void> {
 
 export function onParsingEnd(): void {
   dbService.close();
+  Object.keys(operationIdsRecord).forEach((key) => {
+    delete operationIdsRecord[key];
+  });
 }
 
 export async function saveIndex(indexName: string, objs: { id: number; value: string }[]) {
@@ -30,6 +34,8 @@ export async function saveIndex(indexName: string, objs: { id: number; value: st
 
 const issues: { id: number; issue: any }[] = [];
 const index: { id: number; path: number; method: number; criticality: number }[] = [];
+const operationIdsRecord: Record<string, { path: string; method: string }> = {};
+let idsRecord: Record<string, string> = {};
 
 const methods: Record<string, number> = {
   get: 1,
@@ -44,15 +50,28 @@ const methods: Record<string, number> = {
 
 let issueCount = 0;
 
-export function onIssue(path: number, method: string, issue: any) {
+export function onOperationIssue(operationId: string, issue: any) {
   issues.push({
     id: issueCount,
     issue,
   });
 
-  if (!issue.outcome) {
-    console.info();
-  }
+  index.push({
+    id: issueCount,
+    path: -1,
+    method: -1,
+    criticality: issue.outcome.criticality,
+  });
+
+  idsRecord[issueCount] = operationId;
+  issueCount++;
+}
+
+export function onIssue(path: number, method: string, issue: any) {
+  issues.push({
+    id: issueCount,
+    issue,
+  });
 
   index.push({
     id: issueCount,
@@ -64,13 +83,28 @@ export function onIssue(path: number, method: string, issue: any) {
   issueCount++;
 }
 
-export async function saveIssues() {
+export function onOperationId(operationId: string, path: string, method: string) {
+  operationIdsRecord[operationId] = { path, method };
+}
+
+export async function saveIssues(indexHandler: IndexStore) {
+  for (const entry of index) {
+    // todo: use type prop in future
+    if (entry.path === -1 || entry.method === -1) {
+      const operationId = idsRecord[entry.id];
+      const { path, method } = operationIdsRecord[operationId];
+      entry.path = indexHandler.put("paths", path);
+      entry.method = methods[method];
+    }
+  }
+
   await dbService.bulkPutIssues(issues);
   await dbService.bulkPutIssueIndex(index);
 
   // Clear the issues array after saving, but keep issueCount to avoid reusing IDs
   issues.length = 0;
   index.length = 0;
+  idsRecord = {};
 }
 
 export async function saveMetadata(scanVersion: string, summary: any) {
--- packages/web/src/json-streaming-parser/worker.ts
+++ packages/web/src/json-streaming-parser/worker.ts
@@ -1,26 +1,26 @@
 //// @ts-nocheck
 
-import { TestLogReportWithLocation } from "../app/scan/slice";
 import { IndexStore, makeParser, Parser } from "@xliic/streaming-parser/src/index";
 import {
   addOperations,
-  getScanv2Db,
   initScanv2Db,
   onIssue,
+  onOperationId,
+  onOperationIssue,
   onParsingEnd,
   saveIndex,
   saveIssues,
   saveMetadata,
 } from "./scanv2-processor";
 
-let parser2: Parser;
+let parser: Parser;
 const indexHandler = new IndexStore(["paths"]);
 let operations: any[] = [];
 let scanVersion: string;
 let summary: any;
 
 export async function initProcessReport(dbName: string): Promise<void> {
-  parser2 = makeParser({
+  parser = makeParser({
     "$.shallow()": (value: any) => {
       scanVersion = value.scanVersion;
     },
@@ -29,84 +29,38 @@ export async function initProcessReport(dbName: string): Promise<void> {
       summary = value;
     },
 
-    "$.operations.*.deep()": (value: any, [operationId]: [string]) => {
-      //console.info("op = " + value + ", id " + operationId);
-      const op = value;
-      const path = op.path;
-      const method = op.method;
-      const dbService = getScanv2Db();
-      if (op["conformanceRequestsResults"]) {
-        for (const res of op["conformanceRequestsResults"]) {
-          const issue: TestLogReportWithLocation = {
-            ...res,
-            path,
-            method,
-            testKey: res?.test?.key,
-          };
-          const pathIndex = indexHandler.put("paths", path);
-          onIssue(pathIndex, method, issue);
-        }
-        delete op["conformanceRequestsResults"];
-      }
-      if (op["authorizationRequestsResults"]) {
-        for (const res of op["authorizationRequestsResults"]) {
-          const issue: TestLogReportWithLocation = {
-            ...res,
-            path,
-            method,
-            testKey: res?.test?.key,
-          };
-          const pathIndex = indexHandler.put("paths", path);
-          onIssue(pathIndex, method, issue);
-        }
-        delete op["authorizationRequestsResults"];
-      }
-      if (op["customRequestsResults"]) {
-        for (const res of op["customRequestsResults"]) {
-          const issue: TestLogReportWithLocation = {
-            ...res,
-            path,
-            method,
-            testKey: res?.test?.key,
-          };
-          const pathIndex = indexHandler.put("paths", path);
-          onIssue(pathIndex, method, issue);
-        }
-        delete op["customRequestsResults"];
-      }
-      operations.push({
-        ...op,
-        operationId,
-      });
+    "$.operations.*.shallow()": (value: any, [operationId]: [string]) => {
+      onOperationId(operationId, value.path, value.method.toLowerCase());
     },
 
-    // "$.operations.*.conformanceRequestsResults.*.deep()": (
-    //   value: any,
-    //   [path, method]: [string, string]
-    // ) => {
-    //   console.info("mna = " + value + ", path " + path + ", method " + method);
-    //   // const dbService = getScanv2Db();
-    //   // const issue: TestLogReportWithLocation = {
-    //   //   ...value,
-    //   //   path,
-    //   //   method,
-    //   //   testKey: value?.test?.key,
-    //   // };
-    //   // dbService.addMethodNotAllowedIssue(issue);
-    // },
+    "$.operations.*.deep()": (value: any, [operationId]: [string]) => {
+      delete value["conformanceRequestsResults"];
+      delete value["authorizationRequestsResults"];
+      delete value["customRequestsResults"];
+      operations.push({ ...value, operationId }); // todo: onOperation()
+    },
+
+    "$.operations.*.conformanceRequestsResults.*.deep()": (value: any, [operationId]: [string]) => {
+      onOperationIssue(operationId, value);
+    },
+
+    "$.operations.*.authorizationRequestsResults.*.deep()": (
+      value: any,
+      [operationId]: [string]
+    ) => {
+      onOperationIssue(operationId, value);
+    },
+
+    "$.operations.*.customRequestsResults.*.deep()": (value: any, [operationId]: [string]) => {
+      onOperationIssue(operationId, value);
+    },
 
     "$.methodNotAllowed.*.*.conformanceRequestsResults.*.deep()": (
       value: any,
       [path, method]: [string, string]
     ) => {
-      const issue: TestLogReportWithLocation = {
-        ...value,
-        path,
-        method,
-        testKey: value?.test?.key,
-      };
       const pathIndex = indexHandler.put("paths", path);
-      onIssue(pathIndex, method, issue);
+      onIssue(pathIndex, method, value);
     },
   });
 
@@ -118,15 +72,15 @@ export async function initProcessReport(dbName: string): Promise<void> {
 }
 
 export async function processReport2(done: boolean, value: string): Promise<void> {
-  parser2.chunk(value as string);
+  parser.chunk(value as string);
   if (done) {
-    await saveIssues();
+    await saveIssues(indexHandler);
     await saveMetadata(scanVersion, summary);
     await addOperations(operations);
     for (const bucket of indexHandler.getBuckets()) {
       await saveIndex(bucket, indexHandler.entries(bucket));
     }
-    parser2.end();
+    parser.end();
     onParsingEnd();
     //await onEnd();
   }
