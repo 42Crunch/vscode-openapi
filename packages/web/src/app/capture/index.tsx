67a7906a6c2b434179672a712f9d2495115266c7
Anton Krasovsky <anton@no-mad.net>
2025-07-18 01:06:00 +0100

    Refactoring Capture UI

--- packages/web/src/app/capture/index.tsx
+++ packages/web/src/app/capture/index.tsx
@@ -4,21 +4,49 @@ import { Provider } from "react-redux";
 
 import { Webapp } from "@xliic/common/webapp/capture";
 
-import ThemeStyles from "../../features/theme/ThemeStyles";
 import { ThemeState } from "../../features/theme/slice";
-import { makeWebappMessageHandler } from "../webapp";
+import { RouterContext, Routes } from "../../features/router/RouterContext";
+import { makeWebappMessageHandler, NavigationRouterApp } from "../webapp";
 import { createListener } from "./listener";
 import { initStore, messageHandlers } from "./store";
+import Capture2 from "./Capture2";
 import { RootContainer } from "./Capture";
 
+const routes: Routes = [
+  {
+    id: "starting",
+    title: "",
+    navigation: false,
+    element: <div />,
+  },
+  {
+    id: "capture",
+    title: "Capture",
+    element: <div>Main</div>,
+    children: [
+      {
+        id: "capture2",
+        title: "Capture 2",
+        element: <Capture2 />,
+      },
+      {
+        id: "capture1",
+        title: "Capture 1",
+        element: <RootContainer />,
+      },
+    ],
+  },
+];
+
 function renderWebView(host: Webapp["host"], theme: ThemeState) {
-  const store = initStore(createListener(host), theme);
+  const store = initStore(createListener(host, routes), theme);
 
   createRoot(document.getElementById("root")!).render(
     <React.StrictMode>
       <Provider store={store}>
-        <ThemeStyles />
-        <RootContainer />
+        <RouterContext.Provider value={routes}>
+          <NavigationRouterApp />
+        </RouterContext.Provider>
       </Provider>
     </React.StrictMode>
   );
--- packages/web/src/app/capture/listener.ts
+++ packages/web/src/app/capture/listener.ts
@@ -6,13 +6,31 @@ import {
 import { Webapp } from "@xliic/common/webapp/capture";
 import { AppDispatch, RootState } from "./store";
 import { startListeners } from "../webapp";
-import { browseFiles, downloadFile, openLink, convert, deleteJob } from "./slice";
+import {
+  browseFiles,
+  downloadFile,
+  openLink,
+  convert,
+  deleteJob,
+  showCaptureWindow,
+} from "./slice";
+import { Routes } from "../../features/router/RouterContext";
+import { startNavigationListening } from "../../features/router/listener";
+import { goTo } from "../../features/router/slice";
 
 const listenerMiddleware = createListenerMiddleware();
 type AppStartListening = TypedStartListening<RootState, AppDispatch>;
 const startAppListening = listenerMiddleware.startListening as AppStartListening;
 
-export function createListener(host: Webapp["host"]) {
+export function createListener(host: Webapp["host"], routes: Routes) {
+  const onShowCaptureWindow = () =>
+    startAppListening({
+      actionCreator: showCaptureWindow,
+      effect: async (action, listenerApi) => {
+        listenerApi.dispatch(goTo(["capture", "capture2"]));
+      },
+    });
+
   const listeners: Record<keyof Webapp["hostHandlers"], () => UnsubscribeListener> = {
     browseFiles: () =>
       startAppListening({
@@ -66,6 +84,7 @@ export function createListener(host: Webapp["host"]) {
       }),
   };
 
-  startListeners(listeners);
+  startNavigationListening(startAppListening, routes);
+  startListeners({ ...listeners, onShowCaptureWindow });
   return listenerMiddleware;
 }
