86b0df33b7dc001ff22ecfca89b49c2a08e424b6
Anton Krasovsky <anton@no-mad.net>
2025-11-05 18:03:02 +0000

    Improve test result display

--- packages/web/src/app/scanconf/tests/Test.tsx
+++ packages/web/src/app/scanconf/tests/Test.tsx
@@ -64,12 +64,25 @@ export default function Test({ suite, suiteId }: { suite: SuiteConfiguration; su
 
       {Object.keys(tryResult || {}).length > 0 && (
         <CollapsibleSection title="Result">
-          {Object.entries(tryResult).map(([testId, result]) => (
-            <Execution key={testId} result={result.result} />
-          ))}
+          <Results>
+            <TestTitle>
+              <span>weak-passwords</span>
+              <Passed>Passed</Passed>
+            </TestTitle>
+            {Object.entries(tryResult).map(([testId, result]) => (
+              <TestCardContent key={testId}>
+                <CollapsibleCard>
+                  <Description>{testId}</Description>
+                  <TestCardBody>
+                    <Execution key={testId} result={result.result} />
+                  </TestCardBody>
+                </CollapsibleCard>
+              </TestCardContent>
+            ))}
+          </Results>
         </CollapsibleSection>
       )}
-
+      {/* 
       {failed.length > 0 && (
         <CollapsibleSection title="Failed Tests" defaultOpen>
           <Tests>
@@ -86,7 +99,7 @@ export default function Test({ suite, suiteId }: { suite: SuiteConfiguration; su
             ))}
           </Tests>
         </CollapsibleSection>
-      )}
+      )} */}
     </Container>
   );
 }
@@ -130,6 +143,12 @@ const Tests = styled.div`
   gap: 8px;
 `;
 
+const Results = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
+`;
+
 const TestCardContent = styled.div`
   background-color: var(${ThemeColorVariables.background});
 `;
@@ -152,4 +171,30 @@ export const Description = styled.div`
     gap: 4px;
     align-items: center;
   }
+`;
+
+const TestTitle = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 8px;
+  > span {
+    font-weight: 600;
+  }
+`;
+
+const Passed = styled.div`
+  text-transform: uppercase;
+  padding-left: 1rem;
+  padding-right: 1rem;
+  display: flex;
+  align-items: center;
+  width: 6em;
+  font-size: 0.75rem;
+  border-radius: 4px;
+  font-weight: 450;
+  justify-content: center;
+  padding: 0.25rem 0.5rem;
+  color: var(${ThemeColorVariables.badgeForeground});
+  background-color: var(${ThemeColorVariables.badgeBackground});
 `;
